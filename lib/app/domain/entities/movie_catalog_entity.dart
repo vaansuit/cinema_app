8f6ca5ff703d71cfdbdab5cf87dd1ff1edda1675
unknown <vvansuit@outlook.com>
2022-09-26 16:47:40 -0300

    <feat> added movie_catalog_entity.dart file

---
+++ lib/app/domain/entities/movie_catalog_entity.dart
@@ -0,0 +1,15 @@
+class MovieCatalogEntity {
+  final int movieId;
+  final String movieName;
+  final String posterMovie;
+  final String sinopseMovie;
+  final String trailerMovie;
+
+  MovieCatalogEntity({
+    required this.movieId,
+    required this.movieName,
+    required this.posterMovie,
+    required this.sinopseMovie,
+    required this.trailerMovie,
+  });
+}
