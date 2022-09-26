16b08998a8f16e55851ba0f32287be15b7cf635a
unknown <vvansuit@outlook.com>
2022-09-26 15:32:20 -0300

    <feat> added assets variables and extracted movie poster cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'app/presenter/views/home_page.dart';
+
+void main() => runApp(
+      const ProviderScope(child: Main()),
+    );
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
